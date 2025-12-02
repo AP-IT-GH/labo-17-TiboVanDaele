d051d88499a3e957e41c64141c459f774dec0dd2
s160680 <s160680@ap.be>
2025-12-02 12:24:23 +0100

    oefening gemaakt

---
+++ startbestanden/js/socials.js
@@ -0,0 +1,23 @@
+﻿const socialsListEl = document.querySelector("#socials");
+console.log(socialsListEl);
+const socialPlatforms = ["youtube", "instagram", "facebook", "twitter"];
+const socialLinks = ["https://www.youtube.com", "https://www.instagram.com/", "https://www.facebook.com/", "https://twitter.com/"];
+
+/*for (let i = 0; i < socialPlatforms.length; i++) {
+    document.querySelector("#socials").innerHTML +=
+        `<li>
+            <a href=socialLinks[i]} target="_blank">\n
+                <img src="assets/icon${socialPlatforms[i]}.png"/>
+            </a>
+        </li>`
+}*/
+
+for (let i = 0; i < socialPlatforms.length; i++){
+    const platformEl = document.createElement("li");
+    platformEl.innerHTML = `
+        <a href="${socialLinks[i]}">
+            <img src="./assets/icon${socialPlatforms[i]}.png">
+        </a>
+    `;
+    socialsListEl.appendChild(platformEl);
+}
